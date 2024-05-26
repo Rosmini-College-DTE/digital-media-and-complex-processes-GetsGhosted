8ccdbd10a7bbe5d1638b2e1a0a14b10bb3749132
GetsGhosted <18080@rosmini.school.nz>
2024-05-26 22:31:43 +1200

    worked on running+idle anim
    got running animation for east direction working. downloaded asset only contains animations for one direction.
    
    STILL A TESTING PROJECT

--- .github/Test/Test1/scenes/main_character.gd
+++ .github/Test/Test1/scenes/main_character.gd
@@ -7,11 +7,22 @@ var player_state
 func _physics_process(delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
-	if direction.x == 0 and direction.y ==0:
+	if direction.x == 0 and direction.y == 0:
 		player_state ="idle"
 	elif direction.x !=0 or direction.y != 0:
-		player_state = "walking"
+		player_state = "running"
+	elif direction.x !=1 or direction.y != 1:
+		player_state = "oop-running"
 		
 	velocity = direction * speed
 	move_and_slide()
- 
+	
+	play_anim(direction)
+	
+func play_anim(dir):
+	if player_state == "idle":
+		$Sprite2D.play("idle")
+	if player_state == "running":
+		$Sprite2D.play("running")
+		if dir.x == -1:
+			$Sprite2D.play("opp-running")
