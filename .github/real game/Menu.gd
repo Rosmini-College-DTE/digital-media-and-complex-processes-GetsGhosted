5548dd4433626c8488539ebc00000f22c24f650c
GetsGhosted <18080@rosmini.school.nz>
2024-06-13 10:51:13 +1200

    started working on main menu
    added 3  buttons to main menu

---
+++ .github/real game/Menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+func _on_quit_game_pressed():
+	get_tree().quit()
