b2a84f48b9ae3d0d8b59bcb38ee976487c091289
GetsGhosted <18080@rosmini.school.nz>
2024-06-17 15:08:45 +1200

    play transition to level1

--- .github/real game/Menu.gd
+++ .github/real game/Menu.gd
@@ -1,11 +1,20 @@
 extends Control
 
-func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+@onready var transition = $Transition
+var level1 = preload("res://scenes/level_1.tscn")
 
+func _on_play_pressed():
+	transition.play("fade_out")
+	#get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_transition_animation_finished(_anim_name):
+	get_tree().change_scene_to_packed(level1)
 
 func _on_options_pressed():
 	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
 
 func _on_quit_game_pressed():
 	get_tree().quit()
+
+
+
