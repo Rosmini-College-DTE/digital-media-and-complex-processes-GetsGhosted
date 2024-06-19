421330d157c2a7b0b208128da255c52da71045c1
GetsGhosted <18080@rosmini.school.nz>
2024-06-19 22:44:11 +1200

    updated menu and fixed scaling
    changed images and made moving menu. fixed scale issue hopefully

--- .github/real game/Menu.gd
+++ .github/real game/Menu.gd
@@ -3,6 +3,10 @@ extends Control
 @onready var transition = $Transition
 var level1 = preload("res://scenes/level_1.tscn")
 
+func _process(delta):
+	position += (get_global_mouse_position()*delta)-position
+
+
 func _on_play_pressed():
 	transition.play("fade_out")
 	#get_tree().change_scene_to_file("res://scenes/level_1.tscn")
@@ -15,6 +19,7 @@ func _on_options_pressed():
 
 func _on_quit_game_pressed():
 	get_tree().quit()
+	
 
 
 
--- .github/real game/scenes/options_menu.gd
+++ .github/real game/scenes/options_menu.gd
@@ -1,4 +1,10 @@
 extends Control
 
+@onready var transition = $Transition
+
+func _ready():
+	transition.play("fade_in")
+
 func _on_back_to_menu_pressed():
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
+
