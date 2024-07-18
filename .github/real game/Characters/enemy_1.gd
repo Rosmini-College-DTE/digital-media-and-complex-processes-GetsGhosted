06dbe0190d56c324cf62c65b19f621fecec44e79
GetsGhosted <18080@rosmini.school.nz>
2024-07-18 19:11:36 +1200

    added simple attacks
    need to tweak attacks. tweak cooldowns etc. also need to add attack and damage animations for enemy and player.

--- .github/real game/Characters/enemy_1.gd
+++ .github/real game/Characters/enemy_1.gd
@@ -1,17 +1,23 @@
 extends CharacterBody2D
 
-
-#const SPEED = 300.0
-#const JUMP_VELOCITY = -400.0
-
-var speed = 25
+var speed = 50 #the higher the value, the slower it runs
 var player_chase = false
 var player = null
+
+var health = 80
+var player_inattack_zone = false
+var can_take_damage = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+# Timer to control attack intervals
+var attack_cooldown = 1.0 # in seconds
+var time_since_last_attack = 0.0
+
 func _physics_process(delta):
+	deal_with_damage()
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -27,20 +33,13 @@ func _physics_process(delta):
 	else:
 		$AnimatedSprite2D.play("idle")
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction = Input.get_axis("ui_left", "ui_right")
-	#if direction:
-	#	velocity.x = direction * SPEED
-	#else:
-	#	velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
 
+# Handle attacking
+	time_since_last_attack += delta
+	if player_inattack_zone and time_since_last_attack >= attack_cooldown:
+		attack()
+		time_since_last_attack = 0.0
 
 func _on_detection_area_body_entered(body):
 	$AnimatedSprite2D.play("react")
@@ -48,6 +47,40 @@ func _on_detection_area_body_entered(body):
 	player_chase = true
 
 
-func _on_detection_area_body_exited(body):
+func _on_detection_area_body_exited(_body):
 	player = null
 	player_chase = false
+	
+func enemy_1():
+	pass
+
+func _on_skeleton_hitbox_body_entered(body):
+	if body.has_method("player"):
+		player_inattack_zone = true
+
+func _on_skeleton_hitbox_body_exited(body):
+	if body.has_method("player"):
+		player_inattack_zone = false
+		
+func deal_with_damage():
+	if player_inattack_zone and Globals.player_current_attack == true:
+		if can_take_damage == true:
+			health = health - 20
+			$TakeDamageCooldown.start()
+			can_take_damage = false
+			print("Skeleton HP = ", health)
+			if health <= 0:
+				$AnimatedSprite2D.play("death")
+				self.queue_free()
+
+func _on_take_damage_cooldown_timeout():
+	can_take_damage = true
+
+func attack():
+	print("Attack initiated")  # Added for debugging
+	$AnimatedSprite2D.play("attack")  # Play attack animation
+	# Add logic to deal damage to the player if within attack range
+	if player_inattack_zone and player:
+		# Assume player has a method to take damage
+		if player.has_method("take_damage"):
+			player.take_damage(10) # Example damage value
--- .github/real game/Characters/player.gd
+++ .github/real game/Characters/player.gd
@@ -1,5 +1,12 @@
 extends CharacterBody2D
 
+var enemy_inattack_range = false
+var enemy_attack_cooldown = true
+var health = 100
+var player_alive = true
+
+var attack_ip = false
+
 @export var speed : float = 200.0
 @export var jump_velocity : float = -200.0
 @export var double_jump_velocity : float = -175.0
@@ -13,21 +20,20 @@ var has_double_jumped : bool = false
 var animation_locked : bool = false
 var direction : Vector2 = Vector2.ZERO
 var was_in_air : bool = false
-
-#var attack_type: String
-#var current_attack: bool
-
-#  #func _ready():
-	#current_attack = false
+var current_dir = "none"
 
 func _unhandled_input(_event: InputEvent) -> void:  #interact function
 	if Input.is_action_just_pressed("interact"):
 		var actionables = actionable_finder.get_overlapping_areas()
 		if actionables.size() > 0:
 			actionables[0].action()
-			return
-
+			speed = 0
+	else:
+		speed = 200
+	
 func _physics_process(delta):
+	enemy_attack()
+	attack()
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -35,6 +41,13 @@ func _physics_process(delta):
 	else:
 		has_double_jumped = false
 		was_in_air = false 
+	
+	if health <= 0:
+		player_alive = false #add death menu in future. use get tree function or something
+		health = 0
+		print("You Died") 
+		#$AnimatedSprite2D.play("death")
+		self.queue_free()
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump"):
@@ -45,7 +58,6 @@ func _physics_process(delta):
 			# double jump in air
 			double_jump()
 			
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	direction = Input.get_vector("left", "right", "up", "down")
@@ -57,9 +69,44 @@ func _physics_process(delta):
 	move_and_slide()
 	update_animation()
 	update_facing_direction()
+	#player_movement(delta)
 
+
+func player_movement(delta):
+	
+	if Input.is_action_just_pressed("right"):
+		current_dir = "right"
+		play_anim(1)
+		velocity.x = speed
+	if Input.is_action_just_pressed("left"):
+		current_dir = "left"
+		play_anim(1)
+		velocity.x = speed
+	else:
+		play_anim(0)
+		velocity.x = 0
+
+func play_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("run")
+		elif movement == 0:
+			anim.play("idle")
+	if dir == "left":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("run")
+		elif movement == 0:
+			anim.play("idle")
+	
 func update_animation(): #update animation of character
 	if not animation_locked:
+		if attack_ip:
+			return # Do nothing, keep playing the attack animation
 		if not is_on_floor():
 			animated_sprite.play("jump_loop")
 		else:
@@ -67,7 +114,7 @@ func update_animation(): #update animation of character
 				animated_sprite.play("run")
 			else:
 				animated_sprite.play("idle")
-			
+
 func update_facing_direction(): #change direction character faces
 	if direction.x > 0:
 		animated_sprite.flip_h = false
@@ -86,5 +133,48 @@ func double_jump(): #double jump function
 	has_double_jumped = true
 	
 func _on_animated_sprite_2d_animation_finished(): #jump animation ends once landed
-	if(["jump_end", "jump_start", "jump_double"].has(animated_sprite.animation)):
-		animation_locked = false 
+	if(["jump_end", "jump_start", "jump_double", "attack1"].has(animated_sprite.animation)):
+		animation_locked = false
+		if animated_sprite.animation == "attack1":
+			attack_ip = false
+
+func player():
+	pass
+
+func _on_player_hitbox_body_entered(body):
+	if body.has_method("enemy_1"):
+		enemy_inattack_range = true
+
+func _on_player_hitbox_body_exited(body):
+		if body.has_method("enemy_1"):
+			enemy_inattack_range = false
+
+func enemy_attack():
+	if enemy_inattack_range and enemy_attack_cooldown == true:
+		#$AnimatedSprite2D.play("hit")
+		health = health - 10
+		enemy_attack_cooldown = false
+		$AttackCooldown.start()
+		print(health,"HP")
+	
+func _on_attack_cooldown_timeout():
+	enemy_attack_cooldown = true
+	
+func attack():
+	if Input.is_action_just_pressed("attack") and not attack_ip:
+		Globals.player_current_attack = true
+		attack_ip = true
+		animation_locked = true
+		
+		# Ensure the character faces the correct direction when attacking
+		if direction.x != 0:
+			animated_sprite.flip_h = direction.x < 0
+		
+		$AnimatedSprite2D.play("attack1")
+		$DealAttackTimer.start()
+		
+			
+func _on_deal_attack_timer_timeout():
+	$DealAttackTimer.stop()
+	Globals.player_current_attack = false
+	attack_ip = false
