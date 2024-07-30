ed8d46761ea06e8b229082e6df0833dcbc256f3e
GetsGhosted <18080@rosmini.school.nz>
2024-07-30 23:56:51 +1200

    some level 2 tweaks
    need to complete level 2, and add a end game menu screen.

--- .github/real game/Scripts/deathzone.gd
+++ .github/real game/Scripts/deathzone.gd
@@ -4,7 +4,16 @@ class_name KillZone
 
 @onready var deal_damage_zone = $"."
 
+#@onready var transition = $Transition
+#var deathmenu = preload("res://scenes/deathmenu.tscn")
+
+#func _on_play_pressed():
+#	transition.play("fade_in")
+	#get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
 var damage_to_deal = 10000
 
-func _on_area_entered(body):
+
+func _on_area_entered(_body):
+	#transition.play("fade_out")
 	get_tree().change_scene_to_file("res://scenes/deathmenu.tscn")
